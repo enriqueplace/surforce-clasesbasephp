b5d7f695b7001020e9eedc3d245dd0c11f4e2144
gustavomartinez <gustavomartinez@2dbddc63-9732-0410-a0b9-156113df8070>
2007-06-14 02:01:43 +0000

    Primera version de las clases. Se borran las anteriores tener mal escrito el nombre y por comentarios inapropiados

---
+++ surforce-clasesbasephp/Float.class.php
@@ -0,0 +1,51 @@
+<?
+include_once("classNumber.php");
+
+class Float extends Number{ 
+	private $value;
+	static private $instancia = NULL;
+	
+
+	function __construct($numberFloat) { 
+		try {
+		if (is_numeric($numberFloat)) 
+			$this->value = $numberFloat; 
+		}catch (Exception $e){
+			echo 'Se ha producido la siguiente excepción: ', $e->getMessage(), "\n"; 
+		} 
+	} 
+	function isFloat($varNumber){ 
+		try{ 
+		if (!is_numeric($varNumber)) 
+			return true; 
+     	else 
+			return false; 
+		}catch (Exception $e){ 
+			echo 'Se ha producido la siguiente excepción: ', $e->getMessage(), "\n"; 
+    	} 
+	}
+
+	public function intValue(){ 
+        echo parent:: intValue(); 
+	} 
+	public function toString(){ 
+        return settype($value, "string"); 
+        
+	} 
+	
+	public function floatValue(){ 
+               return parent::floatValue(); 
+    } 
+    public function compare($f2){ 
+       return bccomp("$this->value","$f2",'9'); 
+    } 
+	
+	static public function valueOf($vFloat) {
+       if (self::$instancia == NULL) {
+          self::$instancia = new float($vFloat);
+       }
+       return self::$instancia;
+    }
+} 
+
+?>
