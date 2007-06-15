36fcbda6251052df9c0ed2bdc2a543485ca7ae69
dario.ocles <dario.ocles@2dbddc63-9732-0410-a0b9-156113df8070>
2007-06-15 00:58:21 +0000

    Clase base Number, esta es una implementación de la clase abstracta de JAVA que especifica que métodos deben si o si ser agregados. Se optaron por 2 métodos relativamente importantes.

--- trunk/Number.class.php
+++ trunk/Number.class.php
@@ -1,4 +1,29 @@
-<?
-abstract class number{ 
-} 
+<?php
+
+/**
+ * Clase base para todo tipo de numero (enteros, de coma flotante, etc).
+ *
+ * Esta clase solo define unos métodos bases que deberan ser implementados
+ * en todas las clases hijas.
+ *
+ * @abstract
+ * @access public
+ */
+abstract class Number {
+
+        /**
+         * Devuelve el valor en el tipo float
+         *
+         * @return float Es el valor en float
+         */
+        abstract public function floatValue();
+
+        /**
+         * Devuelve el valor en el tipo integer
+         *
+         * @return integer Es el valor en integer
+         */
+        abstract public function intValue();
+}
+
 ?>
