ac596c55cc54e722330c35f24e83e4c412e7c819
sercomi <sercomi@2dbddc63-9732-0410-a0b9-156113df8070>
2007-06-19 16:48:55 +0000

    Se añaden los métodos toCharArray y Trim a la clase String

--- trunk/String.class.php
+++ trunk/String.class.php
@@ -1,16 +1,31 @@
 <?php
 /**
+ * Fichero de definición de la clase String
+ * 
+ * En este archivo definimos toda la estructura base de la
+ * clase String.
+ * 
+ * @package clasesbasephp
+ */
+
+/**
  * Clase base para el tipo de datos String
  *
  * Esta clase define la clase String con sus
  * atributos y funciones
  *
  * @access public
+ * @package clasesbasephp
  */
 class String {
     //Atributo privado donde se guardará el valor de la cadena
     private $value;
     
+    /**
+     * Constructor de los objetos string
+     * 
+     * @param string $cadena Valor con el que se iniciará el objeto
+     */
 	public function __construct($cadena){
        if (is_string($cadena)){
 			$this->value = $cadena;
@@ -19,17 +34,30 @@ class String {
        }
 	}
     
-	
+	/**
+	 * Devuelve el valor del objeto String
+	 * 
+	 * @return string cadena del objeto
+	 */
 	public function getValue(){
 		return $this->value;
 	}
     
-	
+	/**
+	 * Modifica el valor del objeto String
+	 * 
+	 * @param string $cadena nuevo valor para el objeto
+	 */
 	public function setvalue($cadena) {
 		$this->value = $cadena;
 	}
 
-	
+	/**
+	 * Devuelve el carácter en la posición pasada por parámetro
+	 * 
+	 * @return char Carácter de la posición
+	 * @param integer posición del carácter
+	 */
 	public function charAt ($index){
 	       if ($index>=0 && $index<= (strlen($this->value)-1)){
 				$charMatriz = str_split($this->value);
@@ -39,7 +67,11 @@ class String {
 		   }
 	}
 	
-	
+	/**
+	 * Devuelve la cadena pasada a mayúsculas
+	 * 
+	 * @return string cadena en mayúsculas
+	 */
 	public function toUpperCase() {
 		return strtoupper( $this->value );
 	}
@@ -64,9 +96,41 @@ class String {
         return strlen( $this->value );
     }
     
-	
+	/**
+	 * Concatena el valor del objeto con la cadena pasada por parámetro
+	 * 
+	 * @return string cadena concatenada
+	 * @param string cadena que se añadirá al final
+	 */
 	public function concat ($cadena){
 		return ( $this->value . $cadena );
 	}
+	
+	/**
+	 * Elimina los espacios en blanco del principio y al final
+	 * 
+	 * Devuelve la cadena sin los espacios en blanco, tabuladores, NUL del principio
+	 * y del final de la cadena de texto
+	 * 
+	 * @return string cadena sin espacios en blanco
+	 */
+	public function trim() {
+		return ( trim( $this->value ));
+	}
+	
+
+	/**
+	 * Metodo toCharArray
+	 *
+	 * Devuelve una matriz escalar donde los valores de la misma son los
+	 * caracteres de la cadena pasada como parametro.
+	 *
+	 * @ return array Matriz con los caracteres de la cadena
+	 */
+	  public function toCharArray(){
+	               return str_split($this->value);
+	  }
+
+	
 }
 ?>
