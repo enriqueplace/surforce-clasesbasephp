2e2e4733dd2e90100d296980b6a991fb2d9ed098
garciavlp <garciavlp@2dbddc63-9732-0410-a0b9-156113df8070>
2007-07-01 22:14:18 +0000

    Autor: Aarón García A Fecha: 01/07/2007 Descripción: Se ha modificado la referencia  a la clase number y se ha corregido la asignación al atributo value en el método toString, ya que este referenciaba a value como si fuera una variable local.

--- trunk/Float.class.php
+++ trunk/Float.class.php
@@ -1,51 +1,53 @@
 <?
-include_once("classNumber.php");
+//include_once("classNumber.php");
+include_once("Number.class.php");
 
-class Float extends Number{ 
+class Float extends Number{
 	private $value;
 	static private $instancia = NULL;
-	
 
-	function __construct($numberFloat) { 
+
+	function __construct($numberFloat) {
 		try {
-		if (is_numeric($numberFloat)) 
-			$this->value = $numberFloat; 
+		if (is_numeric($numberFloat))
+			$this->value = $numberFloat;
 		}catch (Exception $e){
-			echo 'Se ha producido la siguiente excepción: ', $e->getMessage(), "\n"; 
-		} 
-	} 
-	function isFloat($varNumber){ 
-		try{ 
-		if (!is_numeric($varNumber)) 
-			return true; 
-     	else 
-			return false; 
-		}catch (Exception $e){ 
-			echo 'Se ha producido la siguiente excepción: ', $e->getMessage(), "\n"; 
-    	} 
+			echo 'Se ha producido la siguiente excepción: ', $e->getMessage(), "\n";
+		}
+	}
+	function isFloat($varNumber){
+		try{
+		if (!is_numeric($varNumber))
+			return true;
+     	else
+			return false;
+		}catch (Exception $e){
+			echo 'Se ha producido la siguiente excepción: ', $e->getMessage(), "\n";
+    	}
 	}
 
-	public function intValue(){ 
-        echo parent:: intValue(); 
-	} 
-	public function toString(){ 
-        return settype($value, "string"); 
-        
-	} 
-	
-	public function floatValue(){ 
-               return parent::floatValue(); 
-    } 
-    public function compare($f2){ 
-       return bccomp("$this->value","$f2",'9'); 
-    } 
-	
+	public function intValue(){
+        echo parent:: intValue();
+	}
+	public function toString(){
+        //return settype($value, "string");
+        return settype($this->value,"string");
+
+	}
+
+	public function floatValue(){
+               return parent::floatValue();
+    }
+    public function compare($f2){
+       return bccomp("$this->value","$f2",'9');
+    }
+
 	static public function valueOf($vFloat) {
        if (self::$instancia == NULL) {
           self::$instancia = new float($vFloat);
        }
        return self::$instancia;
     }
-} 
+}
 
 ?>
